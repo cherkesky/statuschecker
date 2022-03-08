65e385e5ba1d09dcda2d92db44e757d68d2f5a7a
Cherkesky <guy@cherkesky.com>
2022-03-08 14:34:31 -0600

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module statuscheck
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.youtube.com",
+		"https://www.amazon.com",
+		"https://www.cherkesky.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// for {
+
+	for l := range c {
+		go func(l string) {
+			time.Sleep(5 * time.Second)
+			checkLink(l, c)
+		}(l)
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link + " BAD")
+		c <- link
+		return
+	}
+	fmt.Println(link + " GOOD")
+	c <- link
+}
